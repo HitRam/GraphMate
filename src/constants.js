3874d406d1d472630f8e0f5e34f52e6a6012a44d
Hitesh Ramchandani <hitr9831@gmail.com>
2018-01-07 00:41:18 +0530

    Add basic UI

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+/* 
+This file consists of all the constants used in the application
+*/
+
+module.exports = {
+
+	//Default properties of node
+	DEFAULT_NODE_COLOR: "black",
+	DEFAULT_NODE_SHAPE: "circle",
+	DEFAULT_NODE_NAME: "",
+	DEFAULT_NODE_SIZE: 0.5
+}
---
+++ src/node.js
@@ -0,0 +1,13 @@
+let constants = require('./constants.js')
+
+
+/*
+	Properties of node are defined by this function
+*/
+function Node(id, name=constants.DEFAULT_NODE_NAME, color=constants.DEFAULT_NODE_COLOR, shape=constants.DEFAULT_NODE_SHAPE, size=constants.DEFAULT_SIZE) {
+	this.id = id;
+	this.name = name;
+	this.color = color;
+	this.shape = shape;
+	this.size = size;
+}
